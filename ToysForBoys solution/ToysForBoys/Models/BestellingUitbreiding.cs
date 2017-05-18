17910ca8b53a6c1d3c5873b7668e573494da7f46
Bart <Bart@DESKTOP-K8U523O>
2017-05-18 10:45:47 +0200

    Toevoegen totaal property, checken of het product reeds in bestelling zit

---
+++ ToysForBoys solution/ToysForBoys/Models/BestellingUitbreiding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToysForBoys.Models
+{
+    public partial class Bestelling
+    {
+        public decimal Totaal { get {return BerekenTotaal();} }
+
+
+        //berekent het totaal van alle besteldetails in de bestelling
+        private decimal BerekenTotaal()
+        {
+            decimal totaal = decimal.Zero;
+            foreach(var bestelDetail in this.BestelDetails)
+            {
+                totaal += bestelDetail.Totaal;
+            }
+            return totaal;
+        }
+    }
+}
--- ToysForBoys solution/ToysForBoys/Models/Winkelwagen.cs
+++ ToysForBoys solution/ToysForBoys/Models/Winkelwagen.cs
@@ -23,15 +23,35 @@ namespace ToysForBoys.Models
 
         public void VoegBesteldetailsToe(int productId, int aantal)
         {
-            using(var entities= new webshopEntities())
+            using (var entities = new webshopEntities())
             {
+                //Als het een eerste bestelling is moet er een nieuwe collection aangemaakt worden
                 if (Bestelling.BestelDetails == null)
                     this.Bestelling.BestelDetails = new ObservableCollection<BestelDetail>();
+
+                //Er staan reeds besteldetails in de bestelling
                 else
                 {
                     Product product = entities.Producten.Find(productId);
+                    //checken of de productId in de database zit
                     if (product != null)
-                        this.Bestelling.BestelDetails.Add(new BestelDetail { Product = product, Aantal = aantal });
+                    {
+                        //checken of het zelfde product reeds in de bestelling zit..
+                        bool reedsBesteld = false;
+                        foreach (var bestelDetail in this.Bestelling.BestelDetails)
+                        {
+                            //..zo ja - tel het aantal bij op
+                            if (bestelDetail.Product.ID == productId)
+                            {
+                                bestelDetail.Aantal += aantal;
+                                reedsBesteld = true;
+                            }
+                        }
+                        //..zo nee - voeg een nieuw besteldetail toe aan de bestelling
+                        if (!reedsBesteld)
+                            this.Bestelling.BestelDetails.Add(new BestelDetail { Product = product, Aantal = aantal });
+                    }
+                    //verkeerde productId
                     else
                         throw new Exception("Product not found");
                 }
