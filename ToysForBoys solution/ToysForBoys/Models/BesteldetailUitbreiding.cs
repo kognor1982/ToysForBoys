5a032bc58cfdf63900810fa42ea539027f880a4b
Bart <Bart@DESKTOP-K8U523O>
2017-05-18 11:42:42 +0200

    Handmatige Merge winkelwagen

---
+++ ToysForBoys solution/ToysForBoys/Models/BesteldetailUitbreiding.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToysForBoys.Models
+{
+    public partial class BestelDetail
+    {
+        public decimal Totaal { get { return Aantal * BestelPrijs; } }
+    }
+}
