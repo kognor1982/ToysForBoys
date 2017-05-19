051fb1532d9d811dfeb0510165648d5852f18dd9
Yves Cools <Yves Cools@HP>
2017-05-19 09:34:58 +0200

    repair git into master2

---
+++ ToysForBoys v2 solution/ToysForBoys/Models/BestelDetailUitbreiding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToysForBoys.Models
+{
+    public partial class BestelDetail
+    {
+        [DisplayFormat(DataFormatString = "{0:€ #,##0.00}")]
+        public decimal Totaal { get { return Aantal * Product.Prijs; } }
+    }
+}
