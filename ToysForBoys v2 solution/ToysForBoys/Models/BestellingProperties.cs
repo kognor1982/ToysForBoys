3946f7fd240c1ee6799f98eeee2f37b4a423d19f
stevenvh <stevenvh@DESKTOP-EQ1VC2V>
2017-05-29 13:23:37 +0200

    Opkuis

--- ToysForBoys v2 solution/ToysForBoys/Models/BestellingProperties.cs
+++ ToysForBoys v2 solution/ToysForBoys/Models/BestellingProperties.cs
@@ -8,11 +8,11 @@ namespace ToysForBoys.Models
 {
     public class BestellingProperties
     {
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BestelDatum { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime LeverDatum { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime VerzendDatum { get; set; }
     }
 }
